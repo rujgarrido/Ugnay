a45588867532d316590c54fcab77568ccc67695e
rujgarrido <garridoruj@gmail.com>
2026-09-01 17:38:35 +0800

    fix test scripts

--- server/src/lib/crypto.util.ts
+++
@@ -1,9 +0,0 @@
-import crypto from 'crypto';
-
-export function generateRefreshToken(): string {
-  return crypto.randomBytes(32).toString('base64url');
-}
-
-export function hashRefreshToken(token: string): string {
-  return crypto.createHash('sha256').update(token).digest('hex');
-}
